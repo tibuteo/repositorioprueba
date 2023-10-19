69a35537ef0ff7713c9f97918c832410b23ebf06
tibuteo <148298877+tibuteo@users.noreply.github.com>
2023-10-19 22:54:55 +0200

    Push modificado

--- src/System/out/Principal.java
+++ src/System/out/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");
+		System.out.println("Hola mundo2");
 	}
 
 }
