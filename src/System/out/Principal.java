c120fb26ba5d3a030e7e2d8f911bc5f8159c7a0a
tibuteo <148298877+tibuteo@users.noreply.github.com>
2023-10-19 22:35:02 +0200

    Primera confirmación en Eclipse

---
+++ src/System/out/Principal.java
@@ -0,0 +1,10 @@
+package System.out;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module Proyecto_eclipse_git {
+}
